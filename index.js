2436c8cd3d378a6575c6fe2de463fa430876dc8f
Takashi <th2826@columbia.edu>
2021-08-01 23:53:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const messages = []
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards (names, event) {
+    for (let i =0; i < names.length; i++) {
+        messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+//console.log(writeCards(names, 'birthday'));
+
+function countDown (number) {
+    while (number > -1)
+    console.log(number--);
+    number--;
+}
+
+countDown (10);
+
